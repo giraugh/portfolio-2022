bd2daf656f92377088b53a96cfbab5c0565b35a6
Ewan Breakey <ebreakey@gmail.com>
2022-06-19 21:34:42 +1000

    Update automatarium section wording

--- src/sections/Automatarium/Automatarium.tsx
+++ src/sections/Automatarium/Automatarium.tsx
@@ -12,11 +12,15 @@ const Automatarium = () =>
     <div class={styles.Content}>
       <img class={styles.Title} src={automatariumBannerURL} alt='Automatarium Logo and Title'/>
       <div class={styles.Text}>
-        <p>
-        <b>Automatarium</b> is a <b>modern recreation of JFLAP</b>, a dated tool for analysing automata and other theoretical computer science concepts.
-        Automatarium adds a sleek modern user interface, dozens of quality-of-life features and a super easy-to-use testing lab.
-        Originally created as part of my <b>capstone project</b> at RMIT, Automatarium is now <b>open-source</b> and accepting contributions.
-        </p>
+        <div>
+          <p>
+            <b>Automatarium</b> is a <b>modern recreation of JFLAP</b>, a dated tool for analysing automata and other theoretical computer science concepts.
+            Automatarium adds a sleek modern user interface, dozens of quality-of-life features and a super easy-to-use testing lab.
+          </p>
+          <p>
+            Originally created as part of a group <b>capstone project</b> at RMIT, Automatarium is now <b>open-source</b> and accepting contributions.
+          </p>
+        </div>
         <Button href={AUTOMATARIUM_LINK} background='var(--automatarium-brand)'>Take me there! {createElement(ArrowRight)}</Button>
       </div>
       <div class={styles.ScreenshotContainer}>
